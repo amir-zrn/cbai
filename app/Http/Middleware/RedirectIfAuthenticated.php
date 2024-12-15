b4c37bbcc47d867f83a0fe6a73d6518bac18d53d
amir-zrn <amir.zareiyan01@gmail.com>
2024-12-15 13:59:55 +0330

    Replaced with new structure and big changes in file structure

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
+use App\Providers\AppServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,7 +13,9 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param  string|null  ...$guards
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response
      */
     public function handle(Request $request, Closure $next, string ...$guards): Response
     {
@@ -21,7 +23,11 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                if ($request->expectsJson()) {
+                    return response()->json(['message' => 'Already authenticated.'], 200);
+                }
+
+                return redirect(AppServiceProvider::HOME);
             }
         }
 
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\TokenController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -9,11 +12,28 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/sanctum/token', TokenController::class);
+
+Route::middleware(['auth:sanctum', 'apply_locale'])->group(function () {
+
+    /**
+     * Auth related
+     */
+    Route::get('/users/auth', AuthController::class);
+
+    /**
+     * Users
+     */
+    Route::put('/users/{user}/avatar', [UserController::class, 'updateAvatar']);
+    Route::resource('users', UserController::class);
+
+    /**
+     * Roles
+     */
+    Route::get('/roles/search', [RoleController::class, 'search'])->middleware('throttle:400,1');
 });
